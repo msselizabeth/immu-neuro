05abb61a64efd27298418ce8ae812b42b224f8a2
msselizabeth <lizagotovitryby@gmail.com>
2025-04-15 10:42:49 -0400

    updated footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="pt-7 pb-7 pl-4 pr-4 md:p-10 bg-blue-100">
       <p className="text-center text-sm md:text-base">
-        Приватний кабінет лікаря — ФОП "Іму Нейро".
+        Приватний кабінет лікаря — ФОП &quot;Іму Нейро&quot;.
       </p>
       <p className="text-center text-sm md:text-base">
       © {new Date().getFullYear()}. Всі права захищені.
