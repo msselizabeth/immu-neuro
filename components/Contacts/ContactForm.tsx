54dd8d0d6adad99471a5987ccba2e32c2d98382c
msselizabeth <lizagotovitryby@gmail.com>
2025-04-15 11:26:51 -0400

    updated form logic

--- components/Contacts/ContactForm.tsx
+++ components/Contacts/ContactForm.tsx
@@ -3,6 +3,7 @@ import { IMaskInput } from "react-imask";
 import React, { useState } from "react";
 import styles from "./Contacts.module.css";
 import emailjs from "@emailjs/browser";
+import { ClipLoader } from "react-spinners";
 
 interface FormData {
   fullName: string;
@@ -11,26 +12,11 @@ interface FormData {
   comment: string;
 }
 
-// const {
-//   NEXT_PUBLIC_SERVICE_ID,
-//   NEXT_PUBLIC_TEMPLATE_ID,
-//   NEXT_PUBLIC_PUBLIC_KEY,
-// } = process.env;
-
-// if (
-//   !NEXT_PUBLIC_PUBLIC_KEY ||
-//   !NEXT_PUBLIC_SERVICE_ID ||
-//   !NEXT_PUBLIC_TEMPLATE_ID
-// ) {
-//   alert(
-//     "Щось пішло не так. Спробуйте перезавантажити сторінку та повторити спробу."
-//   );
-// }
-
 export default function ContactForm() {
   const [consultationType, setConsultationType] = useState("Онлайн");
   const [errorMessage, setErrorMessage] = useState("");
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [formData, setFormData] = useState<FormData>({
     fullName: "",
     email: "",
@@ -51,23 +37,31 @@ export default function ContactForm() {
       return;
     }
 
-  
+    const serviceId = process.env.NEXT_PUBLIC_SERVICE_ID;
+    const templateId = process.env.NEXT_PUBLIC_TEMPLATE_ID;
+    const key = process.env.NEXT_PUBLIC_PUBLIC_KEY;
+
+    if (!serviceId || !templateId || !key) {
+      alert(
+        "Щось пішло не так. Спробуйте перезавантажити сторінку та повторити спробу."
+      );
+      return;
+    }
+    setIsLoading(true);
     emailjs
-      .sendForm(
-        process.env.NEXT_PUBLIC_SERVICE_ID || "",
-        process.env.NEXT_PUBLIC_TEMPLATE_ID || "",
-        e.currentTarget,
-        {
-          publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY || "",
-        }
-      )
+      .sendForm(serviceId, templateId || "", e.currentTarget, {
+        publicKey: key,
+      })
       .then(
         () => {
           console.log("SUCCESS!");
           setIsSubmitted(true);
+          setIsLoading(false)
+          
         },
         (error) => {
           console.log("FAILED...", error.text);
+          setIsLoading(false);
           setErrorMessage("Не вдалося відправити форму. Спробуйте ще раз.");
         }
       );
@@ -90,11 +84,14 @@ export default function ContactForm() {
     }));
   };
 
-
   return (
     <div className="md:w-[60%] xl:w-[50%] m-auto">
       {isSubmitted ? (
-        <div className={`text-center p-8 text-lg text-[#458FF6] rounded-3xl flex flex-col gap-4 mt-10`}>
+        <div
+          className={`text-center p-8 text-lg text-[#458FF6] rounded-3xl flex flex-col gap-4 mt-10`}>
+             <div className="flex justify-center items-center w-15 h-15 rounded-[50%] border-2 m-auto">
+                <img src="/check-icon.svg" alt="" className="block w-10 h-10"/>
+              </div>
           Дякуємо! Ваш запит прийнято. Очікуйте зв’язку з лікарем протягом 2
           годин.
         </div>
@@ -102,7 +99,8 @@ export default function ContactForm() {
         <form
           onSubmit={handleSubmit}
           className={`my-shadow p-6 rounded-3xl flex flex-col gap-4 mt-10`}>
-          <h2 className="text-xl font-semibold text-[#458FF6]">
+            <h2 className="text-xl font-semibold text-[#458FF6]">
+           
             Заповніть цю форму та лікар з вами сам зв’яжеться
           </h2>
 
@@ -182,11 +180,15 @@ export default function ContactForm() {
           {errorMessage && (
             <p className="text-red-500 text-sm">{errorMessage}</p>
           )}
-          <button
-            type="submit"
-            className="w-full py-3 bg-[#458FF6] text-white rounded-full font-semibold hover:bg-[#BA68C8] transition-all duration-300">
-            Відправити
-          </button>
+          {isLoading ? (
+              <p className="flex items-center justify-center gap-2 text-[#BA68C8] p-3"><ClipLoader size={20} color="#BA68C8"/> Ваш запит оброблюється...</p>
+          ) : (
+            <button
+              type="submit"
+              className="w-full py-3 bg-[#458FF6] text-white rounded-full font-semibold hover:bg-[#BA68C8] transition-all duration-300">
+              Відправити
+            </button>
+          )}
         </form>
       )}
     </div>
